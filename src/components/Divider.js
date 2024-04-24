2a47ab71c4929a9c11bc58b30fc31634708900ab
unknown <dagtt316@gmail.com>
2024-04-24 14:39:07 +0300

    feat: task board initial look

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -6,8 +6,9 @@ const Divider = () => {
       style={{
         width: "100%",
         height: 1,
-        backgroundColor: "gray",
-        margin: "8px 0",
+        backgroundColor: "#000",
+        marginBottom: 12,
+        opacity: 0.7,
       }}
     ></div>
   );

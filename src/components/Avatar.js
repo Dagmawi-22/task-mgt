0d37fdefbdb1903899cd03ba87143b48445d66cd
unknown <dagtt316@gmail.com>
2024-04-24 15:26:46 +0300

    feat: sample list of card groups with dummy data

---
+++ src/components/Avatar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Avatar = ({ title }) => {
+  const colors = ["#FF5733", "#FFBD33", "#33FF6E", "#339FFF", "#B533FF"];
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  const randomColor = getRandomColor();
+
+  return (
+    <div
+      style={{
+        backgroundColor: randomColor,
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        position: "absolute",
+        bottom: 5,
+        right: 5,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <span style={{ color: "#fff", fontSize: 11 }}>{title}</span>
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ src/dummy.js
@@ -0,0 +1,96 @@
+export const data = [
+  {
+    title: "Backlog",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+  {
+    title: "Testing",
+    tasks: [
+      {
+        title: "Applying auth middleware",
+      },
+      {
+        title: "Adding guard to delete user API endpoint",
+      },
+    ],
+  },
+];

49903e6b1d24f0ddd1d1f70cd5f13d8236ce0313
unknown <dagtt316@gmail.com>
2024-04-30 16:15:40 +0300

    update: hide workspace name when logged out

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import TrelloBoard from "./components/Board";
 import { SlPeople } from "react-icons/sl";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Layout from "./components/Layout";
+import { useAtom } from "jotai";
+import { userDataAtom } from "./data/atoms";
 
 function App() {
+  const [user, setUser] = useAtom(userDataAtom);
+
   return (
     <div className="App">
       <Layout>
         <GoogleOAuthProvider clientId="781628626191-9a7sdn64mj0b0eppfv30inp1i7010fb9.apps.googleusercontent.com">
           <header className="App-header">
-            <p style={{ color: "#fff" }}>
-              ABC Workspace
-              <span style={{ cursor: "pointer" }}>
-                <SlPeople className="people-icon" />
-                <span className="badge">5</span>
-              </span>
-            </p>
+            {user && (
+              <p style={{ color: "#fff" }}>
+                ABC Workspace
+                <span style={{ cursor: "pointer" }}>
+                  <SlPeople className="people-icon" />
+                  <span className="badge">5</span>
+                </span>
+              </p>
+            )}
+
             <div className="scroll-container">
               <TrelloBoard />
             </div>
